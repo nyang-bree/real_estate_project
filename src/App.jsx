f6811a5004187c7c41ac4a6efb3d376a79900775
Nyang Brendaline <monkannyang@gmail.com>
2024-02-28 14:00:41 +0100

    create header and style for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './components/Header'
 
  function App  () {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/sign-in' element={<SignIn/>} />
