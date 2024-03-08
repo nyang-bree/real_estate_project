c105cdbe500386cc9216228a18a9e981f45f6ec8
Nyang Brendaline <monkannyang@gmail.com>
2024-03-08 23:52:46 +0100

    sign in page functionalies

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -21,7 +21,7 @@ const SignUp = () => {
             {
                 method:'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
                 body:JSON.stringify(formData),
             });
